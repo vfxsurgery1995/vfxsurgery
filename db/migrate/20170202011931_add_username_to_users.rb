144621df5a59b858e41de0732cc104ef01d4fc96
Sam Mundon <sammundon@me.com>
2017-02-02 01:43:56 +0000

    tried to build username in db and asign unique id's for users but failed, need help to fix

---
+++ db/migrate/20170202011931_add_username_to_users.rb
@@ -0,0 +1,5 @@
+class AddUsernameToUsers < ActiveRecord::Migration
+  def change
+   
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201172148) do
+ActiveRecord::Schema.define(version: 20170202011931) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,6 @@ ActiveRecord::Schema.define(version: 20170201172148) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  
 
 end
